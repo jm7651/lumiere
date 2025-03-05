076b453739e90e206621f62d19afd6994f8b0aec
jm7651 <jmp7651@gmail.com>
2025-03-05 11:10:54 +0900

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+// next.config.js 수정
 const nextConfig = {
   output: "export",
   basePath: "/lumiere",
   images: {
     unoptimized: true,
   },
+  distDir: "docs", // 빌드 출력 폴더를 docs로 변경
 };
 
 module.exports = nextConfig;
