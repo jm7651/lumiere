190ce11a004e81066e5260e6784c6205217e0f10
jm7651 <jmp7651@gmail.com>
2025-03-05 15:17:25 +0900

    commit

--- app/loading/loadingSpinner.tsx
+++ app/loading/loadingSpinner.tsx
@@ -1,10 +1,14 @@
 import styles from "./loading.module.scss";
+import Lottie from "lottie-react";
+import lottieAnimation from "../../public/lumiere/lumiere_lottie.json";
 
 export default function LoadingSpinner() {
   return (
     <div className={styles.loading}>
       <div className="text-center">
-        <div className="animate-spin rounded-full h-16 w-16 border-8 border-gray-200 border-t-blue-600 mx-auto"></div>
+        <div className="mx-auto" style={{ width: "150px", height: "150px" }}>
+          <Lottie animationData={lottieAnimation} loop={true} />
+        </div>
         <p className="mt-4 text-gray-600 font-medium">Loading...</p>
       </div>
     </div>
